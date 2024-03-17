e9e3a49aea59e1fea573b6556ab1940b4becf1bc
saroj kalia <sarojkalia11@gmail.com>
2024-03-17 10:20:56 +0530

    add method

--- src/main/java/com/student/service/StudentService.java
+++ src/main/java/com/student/service/StudentService.java
@@ -2,5 +2,13 @@ package com.student.service;
 
 public class StudentService {
 
-   add{}
+    public void add(int a, int b){
+        System.out.println(a+b);
+    }
+    public void mul(int a, int b){
+        System.out.println(a*b);
+    }
+    public void div(int a, int b){
+        System.out.println(a/b);
+    }
 }
