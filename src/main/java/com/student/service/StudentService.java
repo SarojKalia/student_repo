6ca94b0db218764bd66633539f2f8bb37bbe8849
Saroj Kalia <125301404+SarojKalia@users.noreply.github.com>
2024-03-17 10:21:08 +0530

    Update StudentService.java

--- src/main/java/com/student/service/StudentService.java
+++ src/main/java/com/student/service/StudentService.java
@@ -5,9 +5,7 @@ public class StudentService {
     public void add(int a, int b){
         System.out.println(a+b);
     }
-    public void mul(int a, int b){
-        System.out.println(a*b);
-    }
+ 
     public void div(int a, int b){
         System.out.println(a/b);
     }
